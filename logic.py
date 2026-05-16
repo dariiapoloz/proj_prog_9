601fef6da2214234adc9406a0f7e5b9f20c9ba4a
dariiapoloz <dariiapoloz@knu.ua>
2026-05-16 15:10:59 +0300

    fixed check winner and click

--- logic.py
+++ logic.py
@@ -6,6 +6,7 @@ class CheckersLogic:
         self.board = [[None] * 8 for _ in range(8)]
         self._board_setup()
         self.must_continue = False
+        self.winner = None
 
     def _board_setup(self):
         for row in range(8):
@@ -17,22 +18,16 @@ class CheckersLogic:
                         self.board[row][col] = 'w'
 
     def click(self, row2, col2):
-        if self.must_continue:
-            row1, col1 = self.selected
-            piece = self.board[row1][col1]
-            if self.move(row1, col1, row2, col2, piece):
-                return True
-            return False
-        
         if self.selected:
             row1, col1 = self.selected
             piece = self.board[row1][col1]
 
             if self.move(row1, col1, row2, col2, piece):
-                if self.selected != (row2, col2):
+                if not self.must_continue:
                     self.selected = None
                 return True
-            self.selected = None
+            if not self.must_continue:      
+                self.selected = None
             return True
 
         if self.board[row2][col2] and self.board[row2][col2].lower() == self.turn:
@@ -42,15 +37,25 @@ class CheckersLogic:
         return False
 
     def move(self, row1, col1, row2, col2, piece):
+        if piece is None:
+            return False
+        
         dir_row = row2 - row1
         dir_col = col2 - col1
         direction = -1 if piece.lower() == 'w' else 1
+        
+        if not (0 <= row1 < self.SIZE and 0 <= col1 < self.SIZE):
+            return False
+
+        if not (0 <= row2 < self.SIZE and 0 <= col2 < self.SIZE):
+            return False
 
         if abs(dir_col) == 1 and self.board[row2][col2] is None:
             if dir_row == direction or piece.isupper():
                 self._move_piece(row1, col1, row2, col2, piece)
                 self.must_continue = False
                 self._switch_turn()
+                self.check_winner()
                 return True
 
         if abs(dir_col) == 2 and abs(dir_row) == 2 and self.board[row2][col2] is None:
@@ -58,7 +63,7 @@ class CheckersLogic:
             opponent = self.board[mid_row][mid_col]
 
             if opponent and opponent.lower() != piece.lower():
-                if dir_row == 2 * abs(dir_row) or piece.isupper():
+                if dir_row == 2 * direction or piece.isupper():
                     self.board[mid_row][mid_col] = None
                     self._move_piece(row1, col1, row2, col2, piece)
 
@@ -68,6 +73,7 @@ class CheckersLogic:
                         return True
                     self.must_continue = False
                     self._switch_turn()
+                    self.check_winner()
                     return True
                     
         return False
@@ -84,11 +90,11 @@ class CheckersLogic:
         if not piece:
             return False
 
-        directions = [(-2, -2), (-2, 2), (2, -2), (2, 2)]
+        directions = [(-2, -2), (-2, 2), (2, -2), (2, 2)] 
 
         for dir_row, dir_col in directions:
             new_row, new_col = row + dir_row, col + dir_col
-            if 0 <= new_row < 8 and 0 <= new_col < 8:
+            if 0 <= new_row < self.SIZE and 0 <= new_col < self.SIZE:
                 if self.board[new_row][new_col] is None:
                     mid_row, mid_col = row + dir_row // 2, col + dir_col // 2
                     opponent = self.board[mid_row][mid_col]
@@ -96,9 +102,29 @@ class CheckersLogic:
                     if opponent and opponent.lower() != piece.lower():
                         if piece.isupper():
                             return True
-                        if (piece == 'w' and dir_row < 0) or (piece == 'b' and dir_row > 0):
+                        direction = -1 if piece == 'w' else 1
+                        if dir_row == 2 * direction:
                             return True
         return False
+    
+    def check_winner(self):
+        white_exists = False
+        black_exists = False
 
+        for row in range(self.SIZE):
+            for col in range(self.SIZE):
+                piece = self.board[row][col]
+                if piece:
+                    if piece.lower() == 'w':
+                        white_exists = True
+                    elif piece.lower() == 'b':
+                        black_exists = True
+
+        if not white_exists:
+            self.winner = 'Black'
+        elif not black_exists:
+            self.winner = 'White'
+            
     def _switch_turn(self):
-        self.turn = 'b' if self.turn == 'w' else 'w'
+        self.turn = 'b' if self.turn == 'w' else 'w' 
+        self.check_winner()
